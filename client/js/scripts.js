d0c7178e06fe10e3dbd93f116ee1bc6d9df36af8
Ezequiel Guardia <ezequiel.guardia413@gmail.com>
2024-07-20 21:18:43 -0300

    Reindent scripts.js

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -19,47 +19,47 @@ document.getElementById("checkout-btn").addEventListener("click", function () {
     }))
   };
 
-    fetch(`api/create_preference`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(orderData),
-    })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (preference) {
-        createCheckoutButton(preference.id);
+  fetch(`api/create_preference`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(orderData),
+  })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (preference) {
+    createCheckoutButton(preference.id);
 
-        $(".shopping-cart").fadeOut(500);
-        setTimeout(() => {
-          $(".container_payment").show(500).fadeIn();
-        }, 500);
-      })
+    $(".shopping-cart").fadeOut(500);
+    setTimeout(() => {
+      $(".container_payment").show(500).fadeIn();
+    }, 500);
+  })
 
-  });
+});
 
 function createCheckoutButton(preferenceId) {
 
-// Initialize the checkout
+  // Initialize the checkout
   const bricksBuilder = mercadopago.bricks();
 
-    const renderComponent = async (bricksBuilder) => {
-      if (window.checkoutButton) window.checkoutButton.unmount();
-      await bricksBuilder.create(
-        'wallet',
-        'checkout-btn', // class/id where the payment button will be displayed
-        {
-          initialization: {
-            preferenceId: preferenceId
-          },
-          callbacks: {
-            onError: (error) => console.error(error),
-            onReady: () => {}
-          }
+  const renderComponent = async (bricksBuilder) => {
+    if (window.checkoutButton) window.checkoutButton.unmount();
+    await bricksBuilder.create(
+      'wallet',
+      'checkout-btn', // class/id where the payment button will be displayed
+      {
+        initialization: {
+          preferenceId: preferenceId
+        },
+        callbacks: {
+          onError: (error) => console.error(error),
+          onReady: () => {}
         }
-      );
-    };
-    window.checkoutButton =  renderComponent(bricksBuilder);
-  }
+      }
+    );
+  };
+  window.checkoutButton =  renderComponent(bricksBuilder);
+}
